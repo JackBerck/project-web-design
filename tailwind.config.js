edb6f3a09e38d8f0778ccf6891901e987fb75e99
jackberck <berckjack@gmail.com>
2024-05-31 23:22:32 +0700

    style: update styles on projects page and services page

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
         primary: "#006769",
         secondary: "#64748b",
         dark: "#0f172a",
+        light: "rgb(241 245 249)",
       },
       screens: {
         "2xl": "1320px",
